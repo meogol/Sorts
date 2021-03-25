8f439dc7cb54fee03a6a612e3db691a5e0e069bc
EvgeniiKarelin <eugenefolkast@gmail.com>
2021-03-25 18:43:33 +0300

    NormShit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,28 +5,21 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+
 import java.io.IOException;
 
-/**
- * JavaFX App
- */
-
-public class App extends Application {
+public class Main extends Application {
 
     @Override
-    public void start(Stage stage) {
-        try {
-            Parent root = FXMLLoader.load(getClass().getResource("gui.fxml"));
+    public void start(Stage stage) throws IOException {
+            Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("sample.fxml"));
             stage.setTitle("Sorts");
             stage.setScene(new Scene(root,640,480));
             stage.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
     public static void main(String[] args) {
-        launch(args);
+        launch();
     }
 
 }
