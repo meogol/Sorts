822a9723d4d292a217bf60c56796b546d6ef7af9
meogol <avs99.98@gmail.com>
2021-03-25 13:16:20 +0300

    Add sort code

---
+++ src/main/java/org/example/Sort/ShakerSortMethod.java
@@ -0,0 +1,48 @@
+package org.example.Sort;
+
+import java.util.ArrayList;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+public class ShakerSortMethod implements Consumer, SortInterface {
+    public void sort(int[] array) {
+        int left = 0; // левая граница
+        int right = array.length - 1; // правая граница
+
+        do
+        {
+            //Сдвигаем к концу массива "тяжелые элементы"
+            for (int i = left; i < right; i++)
+            {
+                if(array[i] > array[i+1])
+                {
+                    array[i] ^= array[i+1];
+                    array[i+1] ^= array[i];
+                    array[i] ^= array[i+1];
+                }
+            }
+            right--; // уменьшаем правую границу
+            //Сдвигаем к началу массива "легкие элементы"
+            for (int i = right; i > left ; i--)
+            {
+                if(array[i] < array[i-1])
+                {
+                    array[i] ^= array[i-1];
+                    array[i-1] ^= array[i];
+                    array[i] ^= array[i-1];
+                }
+            }
+            left++; // увеличиваем левую границу
+        } while (left <= right);
+    }
+
+
+
+    @Override
+    public void accept(Object o) {
+        if(o.getClass() != int[].class)
+            return;
+
+        sort((int[]) o);
+    }
+}
---
+++ src/main/java/org/example/Sort/ShellSortMethod.java
@@ -0,0 +1,53 @@
+package org.example.Sort;
+
+import java.util.function.Consumer;
+
+public class ShellSortMethod implements Consumer {
+
+    public void sort(int[] array, int leftBorder, int rightBorder) {
+        int leftMarker = leftBorder;
+        int rightMarker = rightBorder;
+        int pivot = array[(leftMarker + rightMarker) / 2];
+        do {
+            // Двигаем левый маркер слева направо пока элемент меньше, чем pivot
+            while (array[leftMarker] < pivot) {
+                leftMarker++;
+            }
+            // Двигаем правый маркер, пока элемент больше, чем pivot
+            while (array[rightMarker] > pivot) {
+                rightMarker--;
+            }
+            // Проверим, не нужно обменять местами элементы, на которые указывают маркеры
+            if (leftMarker <= rightMarker) {
+                // Левый маркер будет меньше правого только если мы должны выполнить swap
+                if (leftMarker < rightMarker) {
+                    int tmp = array[leftMarker];
+                    array[leftMarker] = array[rightMarker];
+                    array[rightMarker] = tmp;
+                }
+                // Сдвигаем маркеры, чтобы получить новые границы
+                leftMarker++;
+                rightMarker--;
+            }
+        } while (leftMarker <= rightMarker);
+
+        // Выполняем рекурсивно для частей
+        if (leftMarker < rightBorder) {
+            sort(array, leftMarker, rightBorder);
+        }
+        if (leftBorder < rightMarker) {
+            sort(array, leftBorder, rightMarker);
+        }
+    }
+
+
+
+
+    @Override
+    public void accept(Object o) {
+        if(o.getClass() != int[].class)
+            return;
+
+        sort((int[]) o, 0, ((int[]) o).length-1);
+    }
+}
