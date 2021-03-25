b61ab673cebb2e8d9cc910071465e023806248de
EvgeniiKarelin <eugenefolkast@gmail.com>
2021-03-25 13:27:50 +0300

    Trying to make it word

---
+++ src/main/java/org/example/Controller.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+
+public class Controller {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button AboutProgrammButton;
+
+    @FXML
+    private ComboBox<?> ChooseSortMethod;
+
+    @FXML
+    private Button StartButton;
+
+    @FXML
+    private Button ShowGrafButton;
+
+    @FXML
+    void initialize() {
+    }
+}
+
